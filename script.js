0c3f75b015c1c0f8fc9be216d40960a612d1bfe7
ARodolfo-Nunes <rodolfomoutonuno13@gmail.com>
2025-07-10 19:05:59 -0300

    Adicionei mais botões

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const buttons = document.querySelectorAll("button");
 const hora = document.getElementById("hora");
 
 //Span para marcar a hora
-const currentTime = () => {hora.textContent = new Date().toLocaleTimeString()};
+const currentTime = () => {
+  hora.textContent = new Date().toLocaleTimeString();
+};
 
 //Mostra a hora assim que carrega
 currentTime();
@@ -24,13 +26,15 @@ buttons.forEach(button => {
         const expression = display.value
           .replace(/×/g, "*")
           .replace(/÷/g, "/")
-          .replace(/–/g, "-");
+          .replace(/–/g, "-").replace(/\^/g,"**");
         display.value = eval(expression);
       } catch (error) {
         display.value = "Error";
       }
     } else if (valor === "C") {
       display.value = "";
+    } else if (valor === "delete") {
+      display.value = display.value.slice(0, -1);
     } else {
       if (display.value === "0" || display.value === "Error") {
         display.value = valor;
